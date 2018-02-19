3651b981a1cbeb336b06414e6d44fde0ac5dbfac
iCode18 <afshinalavi@Afshins-iMac.local>
2018-02-19 21:18:17 +0100

    capture url and render suit page

--- afshinsblog/afshinsblog/urls.py
+++ afshinsblog/afshinsblog/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
 # use include to pack each urls into own app url file
 urlpatterns = [
     path('malekomodir/', admin.site.urls),
     path('', include('posts.urls')),
+    path('posts/(?P<post_id>\d+)/', include('posts.urls')),
 ]
--- afshinsblog/posts/urls.py
+++ afshinsblog/posts/urls.py
@@ -1,4 +1,5 @@
 # use separated url file to have clean and more manageable
+from django.conf.urls import url
 from django.urls import path
 from . import views
 from django.conf.urls.static import static
@@ -6,4 +7,5 @@ from django.conf import settings
 # last line make it possible to render pictures
 urlpatterns = [
     path('', views.home, name='home'),
+    url(r'posts/(?P<post_id>\d+)/$', views.post_details),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
