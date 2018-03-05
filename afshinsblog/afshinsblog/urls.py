7e89818492e4a1e74fdf538002aab16aa3559079
iCode18 <alavi.afshin@gmail.com>
2018-03-05 20:47:44 +0100

    added about pages

--- afshinsblog/afshinsblog/urls.py
+++ afshinsblog/afshinsblog/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+import sitepages.views
 # use include to pack each urls into own app url file
 urlpatterns = [
     path('malekomodir/', admin.site.urls),
     path('', include('posts.urls')),
+    path('about/', sitepages.views.about,name='about'),
     path('posts/(?P<post_id>\d+)/', include('posts.urls')),
 ]
---
+++ afshinsblog/sitepages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def about(request):
+    return render(request, 'sitepages/about.html')
+
+
+
